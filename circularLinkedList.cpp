cf312cede7495797a97f1aabca90c95bb744cbbf
dhananjay9309 <59719022+dhananjay9309@users.noreply.github.com>
2021-02-12 09:13:56 +0530

    Create circularLinkedList.cpp

---
+++ circularLinkedList.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+struct Node 
+{ 
+    int data; 
+    struct Node *next; 
+}; 
+struct Node *addEnd(struct Node *last, int data) 
+{ 
+    if (last == NULL) 
+    {
+        struct Node *temp = new Node; 
+        temp -> data = data; 
+        last = temp; 
+        last -> next = last; 
+        return last; 
+    }
+    
+    struct Node *temp = new Node;
+    temp -> data = data; 
+    temp -> next = last -> next; 
+    last -> next = temp; 
+    last = temp; 
+
+    return last; 
+} 
+void traverse(struct Node *last) 
+{ 
+    struct Node *p; 
+    if (last == NULL) 
+    { 
+        cout << "List is empty." << endl; 
+        return; 
+    } 
+
+    p = last -> next; 
+
+    do
+    { 
+        cout << p -> data << " "; 
+        p = p -> next; 
+
+    } 
+    while(p != last->next); 
+
+} 
+
+int main() 
+{ 
+    struct Node *last = NULL; 
+
+    last = addEnd(last, 6); 
+    last = addEnd(last, 4); 
+    last = addEnd(last, 2); 
+    last = addEnd(last, 8); 
+    last = addEnd(last, 12); 
+    last = addEnd(last, 10); 
+
+    traverse(last); 
+
+    return 0; 
+}
